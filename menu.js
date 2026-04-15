472eecf0d5ed1466c06fbca192c748c0a2d2d307
Laboratorio 02 Sede Surco <lab02-sur@certus.edu.pe>
2026-04-14 22:30:20 -0500

    new update

--- menu.js
+++ menu.js
@@ -1,16 +1,16 @@
 let NormalMenu = [
-    { imagen: "img/obj1.png", titulo: "Nombre del Cafe", descripcion: "Descripcion del cafe", precio: "$10.00", id: "cafe001"},
-    { imagen: "img/obj1.png", titulo: "Nombre del Cafe", descripcion: "Descripcion del cafe", precio: "$10.00", id: "cafe002"},
-    { imagen: "img/obj1.png", titulo: "Nombre del Cafe", descripcion: "Descripcion del cafe", precio: "$10.00", id: "cafe003"},
-    { imagen: "img/obj1.png", titulo: "Nombre del Cafe", descripcion: "Descripcion del cafe", precio: "$10.00", id: "cafe004"}
+    { imagen: "img/obj1.png", titulo: "Nombre del Cafe", descripcion: "Descripcion del cafe", precio: "$10.00", id: "cafe001", btn: "btn001", btnn: "btnn001"},
+    { imagen: "img/obj1.png", titulo: "Nombre del Cafe", descripcion: "Descripcion del cafe", precio: "$10.00", id: "cafe002", btn: "btn002", btnn: "btnn002"},
+    { imagen: "img/obj1.png", titulo: "Nombre del Cafe", descripcion: "Descripcion del cafe", precio: "$10.00", id: "cafe003", btn: "btn003", btnn: "btnn003"},
+    { imagen: "img/obj1.png", titulo: "Nombre del Cafe", descripcion: "Descripcion del cafe", precio: "$10.00", id: "cafe004", btn: "btn004", btnn: "btnn004"}
 ];
 
 let EspecialesMenu = [
-    { imagen: "img/obj1.png", titulo: "Nombre del Cafe", descripcion: "Descripcion del cafe", precio: "$10.00", id: "cafe101"},
-    { imagen: "img/obj1.png", titulo: "Nombre del Cafe", descripcion: "Descripcion del cafe", precio: "$10.00", id: "cafe102"}
+    { imagen: "img/obj1.png", titulo: "Nombre del Cafe", descripcion: "Descripcion del cafe", precio: "$10.00", id: "cafe101", btn: "btn101", btnn: "btnn101"},
+    { imagen: "img/obj1.png", titulo: "Nombre del Cafe", descripcion: "Descripcion del cafe", precio: "$10.00", id: "cafe102", btn: "btn102", btnn: "btnn102"}
 ];
 
-let IdList = [];
+let IdList = {};
 
 let MenuReference = document.getElementById("n-menu");
 let MenuEspeciales = document.getElementById("e-menu");
@@ -34,7 +34,8 @@ for (let data of NormalMenu) {
                     <h4>
                         ${data.precio}
                     </h4>
-                    <button class="add-button" onclick="buttons()">
+                    <button class="quit-button" onclick="quitButtons(this)" id=${data.btnn}></button>
+                    <button class="add-button" onclick="buttons(this)" id=${data.btn}>
                         <img src="https://www.svgrepo.com/show/423634/add-square.svg" class="svg-icon">
                     </button>
                 </div>
@@ -46,7 +47,7 @@ for (let data of NormalMenu) {
     
     TextMenu += TextHtml;
 
-    IdList.push(data.id)
+    IdList[`${data.btn}`] = 0;
 }
 
 for (let data of EspecialesMenu) {
@@ -65,7 +66,8 @@ for (let data of EspecialesMenu) {
                     <h4>
                         ${data.precio}
                     </h4>
-                    <button class="add-button" onclick="buttons()">
+                    <button class="quit-button" onclick="quitButtons(this)" id=${data.btnn}></button>
+                    <button class="add-button" onclick="buttons(this)" id=${data.btn}>
                         <img src="https://www.svgrepo.com/show/423634/add-square.svg" class="svg-icon">
                     </button>
                 </div>
@@ -77,7 +79,7 @@ for (let data of EspecialesMenu) {
     
     TextEspe += TextHtml;
 
-    IdList.push(data.id)
+    IdList[`${data.btn}`] = 0;
 }
 
 MenuReference.innerHTML = TextMenu;
@@ -87,9 +89,31 @@ let Ncarrito = document.getElementById("n-carrito");
 
 let contador = 0;
 
-function buttons() {
+function IDbutton(btn) {
+
+    let cantidad = IdList[`${btn}`];
+    cantidad++;
+    IdList[`${btn}`] = cantidad
+    console.log(btn, " ", IdList[`${btn}`])
+
+    let reference = document.getElementById(btn);
+
+    TextHtml =``
+
+    if(IdList[`${btn}`] > 0) {
+        TextHtml =`
+        <p>${cantidad}</p>
+        <img src="https://www.svgrepo.com/show/423634/add-square.svg" class="svg-icon">`
+    }
+
+    reference.innerHTML = TextHtml;
+}
+
+function buttons(boton) {
     contador++;
     console.log(contador);
+
+    IDbutton(boton.id)
 
     TextHtml =`
         <h3 class="carrito-contador pixel-font">${contador}</h3>
@@ -98,4 +122,22 @@ function buttons() {
         </div>`;
 
     Ncarrito.innerHTML = TextHtml;
+
+    console.log(IdList)
+}
+
+function quitButtons(boton) {
+    contador--;
+    TextHtml =``;
+
+    let reference = document.getElementById(boton.id);
+
+    if (contador > 0) {
+        console.log(contador)
+        TextHtml =`<img src="https://www.svgrepo.com/show/522962/minus-square.svg" class="svg-icon">`
+        reference.innerHTML = TextHtml;
+    }else{
+        TextHtml =``;
+        reference.innerHTML = TextHtml;
+    }
 }
